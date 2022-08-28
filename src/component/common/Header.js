8aed31604429a4d65c99dc54dbfcaae00406acd1
jiyun <sprare06@gmail.com>
2022-08-28 22:35:20 +0900

    header 메인, 서브 구조

--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -1,7 +1,8 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 
 function Header(props) {
   const assetUrl = process.env.PUBLIC_URL
+  const back = useHistory();
 
   return (
     <header className={`header ${props.type}`}>
@@ -10,6 +11,9 @@ function Header(props) {
           <NavLink to="/">
             <img src={`${assetUrl}/assets/img/logo.png`} alt="알약 로고" />
           </NavLink>
+        </div>
+        <div className="header__back" onClick={() => { back.goBack() }}>
+          <img src={`${assetUrl}/assets/img/back.png`} alt="뒤로가기" />
         </div>
         <div className="header__page">
           {props.name}
